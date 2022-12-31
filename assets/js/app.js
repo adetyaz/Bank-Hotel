d045730acce837912c33ff20d3103f6e2ca4e0ea
adetyaz <tayo.lasisi@yahoo.com>
2022-12-31 18:19:34 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+window.addEventListener('onload', () => {
+	console.log('Present Sire')
+})
---
+++ dist/index.fbb7b822.js
@@ -0,0 +1,5 @@
+window.addEventListener("onload", ()=>{
+    console.log("Present Sire");
+});
+
+//# sourceMappingURL=index.fbb7b822.js.map
